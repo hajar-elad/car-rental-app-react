916f1a5b8fa302157b5531ffd0b71fe19703d372
hajar elad <hajar.elad@gmail.com>
2026-03-14 14:45:00 +0100

    Adding Router Basename

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route index element={<Home />} />
+      <Route index element={<Home />}/>
       <Route path="/luxurycarspage" element={<LuxuryCarsPage category = 'LuxuryCars'/>} />
       <Route path="/regularcarspage" element={<RegularCarsPage category = 'RegularCars'/>} />
       <Route path="/checkout" element={<Checkout />} />
