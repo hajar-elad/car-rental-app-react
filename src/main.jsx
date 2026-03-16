8b8a9a2ddbf1ae73d59baddb8b0593b35bd68cda
hajar elad <hajar.elad@gmail.com>
2026-03-16 22:05:48 +0100

    basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/car-rental-app-react/">
       <App/>
     </BrowserRouter>
   </StrictMode>,
